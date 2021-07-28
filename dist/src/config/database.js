2ce024d409507a7e41f1b894cda2a9d50ddce40e
Islam Rustamov <islamrust0@gmail.com>
2021-07-28 13:58:25 +0300

    Dockerize database

--- dist/src/config/database.js
+++ dist/src/config/database.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
 const dotenv_1 = __importDefault(require("dotenv"));
+const pg_1 = require("pg");
 dotenv_1.default.config();
 const pool = new pg_1.Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
 });
 pool.on('connect', () => {
     console.log('Connection established!');
--- dist/src/createApp.js
+++ dist/src/createApp.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
 const todo_routes_1 = __importDefault(require("./routes/todo.routes"));
 const app = express_1.default();
-function default_1(database) {
+exports.default = (database) => {
     app.use(express_1.default.urlencoded({ extended: true }));
     app.use(express_1.default.json());
     app.use(express_1.default.json({ type: 'application/vnd.api+json' }));
@@ -16,6 +16,5 @@ function default_1(database) {
     app.use(routes_1.default);
     app.use(todo_routes_1.default(database));
     return app;
-}
-exports.default = default_1;
+};
 //# sourceMappingURL=createApp.js.map
